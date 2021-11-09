cfbe23a1295b3269a049ccc14cd6bbb6c10ddff3
Khair Al Anam <84736189+khairalanam@users.noreply.github.com>
2021-11-09 20:14:54 +0300

    First version of the game

--- main.py
+++ main.py
@@ -24,17 +24,33 @@ def get_number_of_racers():
             print("Number not in range! Please try again!")
 
 
+def race(colors):
+    turtles = create_turtles(colors)
+
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10:
+                return colors[turtles.index(racer)]
+
+
 def create_turtles(colors):
     turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
     for i, color in enumerate(colors):
         racer = turtle.Turtle()
         racer.color(color)
         racer.shape('turtle')
         racer.left(90)
         racer.penup()
-        racer.setpos()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
         racer.pendown()
         turtles.append(racer)
+
+    return turtles
 
 
 def init_turtle():
@@ -48,4 +64,6 @@ init_turtle()
 
 random.shuffle(COLORS)
 colors = COLORS[:racers]
-create_turtles(colors)
+winner = race(colors)
+print(f"{winner} is the winner!")
+time.sleep(15)
